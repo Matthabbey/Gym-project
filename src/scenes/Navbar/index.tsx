c6860d5067dc18f98b426a27d3c0f2a2fd8945b1
Akinpelu Abiodun <59386529+Matthabbey@users.noreply.github.com>
2023-02-19 02:24:24 +0100

    usemediaQuery

--- src/scenes/Navbar/index.tsx
+++ src/scenes/Navbar/index.tsx
@@ -1,58 +1,69 @@
-import React from "react";
+import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from "@/assets/Logo.png";
 import Link from "./Link";
 import { SelectedPage } from "@/share/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
 
 interface Props {
-    selectedPage: SelectedPage,
-    setSelectedPage: (value: SelectedPage)=>void
-
+  selectedPage: SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
 }
 
-const index = ({selectedPage, setSelectedPage}: Props) => {
+const index = ({ selectedPage, setSelectedPage }: Props) => {
   const flexBetween = "flex items-center justify-center";
+  const isAboveMediumScreen = useMediaQuery("(min-width: 1060px)");
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
   console.log();
-  
+
   return (
     <nav>
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
-          <div className={`${flexBetween} gap-18 w-full`}>
+
+          <div className={`${flexBetween} w-full gap-16 `}>
             {/* LEFT SIDE */}
-            <img src={Logo} alt="" />
+          <img src={Logo} alt="" />
 
             {/* RIGHT SIDE */}
-            <div className={`${flexBetween} w-full`}>
-              <div className={`${flexBetween} gap-8 text-sm`}>
-                <Link
-                  page="Home"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                
+            {isAboveMediumScreen ? (
+              <div className={`${flexBetween} w-full `}>
+                <div className={`${flexBetween} gap-8 text-sm `}>
+                  <Link
+                    page="Home"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
 
-                <Link
-                  page="Benefits"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Our Classes"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
-                <Link
-                  page="Contact Us"
-                  selectedPage={selectedPage}
-                  setSelectedPage={setSelectedPage}
-                />
+                  <Link
+                    page="Benefits"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Our Classes"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                  <Link
+                    page="Contact Us"
+                    selectedPage={selectedPage}
+                    setSelectedPage={setSelectedPage}
+                  />
+                </div>
+                <div className={`${flexBetween}`}>
+                  <p>Sign In</p>
+                  <button>Become a Member</button>
+                </div>
               </div>
-              <div className={`${flexBetween} gap-8`}>
-                <p>Sign In</p>
-                <button>Become a Member</button>
-              </div>
-            </div>
+            ) : (
+              <button
+                className="rounded-full bg-secondary-400 p-2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <Bars3Icon className="h-6 w-6 text-white" />
+              </button>
+            )}
           </div>
         </div>
       </div>
